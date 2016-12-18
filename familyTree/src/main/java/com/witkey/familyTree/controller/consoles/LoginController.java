fb10e744117525ec38fd10cfcdc0fade0f04ef5c
yaqueyy147 <yaqueyy147@sina.com>
2016-12-18 23:41:40 +0800

    20161218

--- familyTree/src/main/java/com/witkey/familyTree/controller/consoles/LoginController.java
+++ familyTree/src/main/java/com/witkey/familyTree/controller/consoles/LoginController.java
@@ -1,4 +1,4 @@
-package com.witkey.familyTree.controller;
+package com.witkey.familyTree.controller.consoles;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
